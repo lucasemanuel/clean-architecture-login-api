e8f3deba05a69b36e55200afd824cc709c13c252
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-11 10:45:06 -0300

    ensure LoginRouter return 401 when invalid credentialsare provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -59,9 +59,19 @@ describe('Login Router', () => {
         password: 'any_password'
       }
     }
-
     sut.route(httpRequest)
     expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
     expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
+  })
+  test('Should return 401 when invalid credentials are provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@email.com',
+        password: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(401)
   })
 })
