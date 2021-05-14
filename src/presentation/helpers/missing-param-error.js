780ce82d2d6a4c66ab8c41ae4157bd079048c95d
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-14 16:50:00 -0300

    fix typing error name

--- src/presentation/helpers/missing-param-error.js
+++ src/presentation/helpers/missing-param-error.js
@@ -1,6 +1,6 @@
 module.exports = class MissingParamError extends Error {
   constructor (paramName) {
     super(`Missing param: ${paramName}`)
-    this.name = 'MissingPAramError'
+    this.name = 'MissingParamError'
   }
 }
