fc7b32c96070cde4325a08851492986e33e290fb
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-23 17:43:36 -0300

    ensure AuthUseCase call LoadUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.test.js
+++ src/domain/usecases/auth-usecase.test.js
@@ -1,9 +1,15 @@
 const { MissingParamError } = require('../../utils/errors')
 
 class AuthUseCase {
+  constructor (loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) throw new MissingParamError('email')
     if (!password) throw new MissingParamError('password')
+
+    await this.loadUserByEmailRepository.load(email)
   }
 }
 
@@ -17,5 +23,16 @@ describe('Auth Usecase', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth('any_email@email.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+  test('Should call LoadUserByEmailRepository with correct email', async () => {
+    class LoadUserByEmailRepositorySpy {
+      async load (email) {
+        this.email = email
+      }
+    }
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+    const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+    sut.auth('any_email@email.com', 'any_password')
+    expect(loadUserByEmailRepositorySpy.email).toBe('any_email@email.com')
   })
 })
