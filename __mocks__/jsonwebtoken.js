b29687f9bd2d9e811bd3ef5e201b27428225135b
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-06-01 22:28:03 -0300

    ensure call JWT with corrects values

--- __mocks__/jsonwebtoken.js
+++ __mocks__/jsonwebtoken.js
@@ -1,7 +1,12 @@
 module.exports = {
   token: 'any_token',
+  payload: '',
+  secret: '',
 
   async sign (payload, secret) {
+    this.payload = payload
+    this.secret = secret
+
     return this.token
   }
 }
