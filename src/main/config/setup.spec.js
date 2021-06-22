14b38ad4ada58511e3b2d6e857bd30a7c083a93f
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-06-22 16:33:38 -0300

    move test cors to its own file

--- src/main/config/setup.spec.js
+++ src/main/config/setup.spec.js
@@ -5,13 +5,7 @@ describe('App Setup', () => {
   test('Should disable x-powered-by header', async () => {
     app.get('/test_x_powered_by', (req, res) => res.send(''))
     const response = await request(app).get('/test_x_powered_by')
+
     expect(response.headers['x-powered-by']).toBeUndefined()
-  })
-  test('Should enable CORS', async () => {
-    app.get('/test_cors', (req, res) => res.send(''))
-    const response = await request(app).get('/test_cors')
-    expect(response.headers['access-control-allow-origin']).toBe('*')
-    expect(response.headers['access-control-allow-methods']).toBe('*')
-    expect(response.headers['access-control-allow-headers']).toBe('*')
   })
 })
