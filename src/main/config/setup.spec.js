d07d640f0354cce6082e7c432417dfffac8887ac
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-06-22 16:26:27 -0300

    enable CORS

--- src/main/config/setup.spec.js
+++ src/main/config/setup.spec.js
@@ -7,4 +7,11 @@ describe('App Setup', () => {
     const response = await request(app).get('/test_x_powered_by')
     expect(response.headers['x-powered-by']).toBeUndefined()
   })
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => res.send(''))
+    const response = await request(app).get('/test_cors')
+    expect(response.headers['access-control-allow-origin']).toBe('*')
+    expect(response.headers['access-control-allow-methods']).toBe('*')
+    expect(response.headers['access-control-allow-headers']).toBe('*')
+  })
 })
