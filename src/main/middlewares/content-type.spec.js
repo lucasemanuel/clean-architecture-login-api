47f1fb17cf7c24969bb7b44ecadae9287bf538f8
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-06-22 16:59:12 -0300

    ensure requests return given content-type when forced

--- src/main/middlewares/content-type.spec.js
+++ src/main/middlewares/content-type.spec.js
@@ -8,4 +8,14 @@ describe('Content-Type Middleware', () => {
       .get('/test_content_type')
       .expect('content-type', /json/)
   })
+  test('Should return xml content-type if forced', async () => {
+    app.get('/test_content_type_xml', (req, res, next) => {
+      res.type('xml')
+      res.send('')
+      next()
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
 })
