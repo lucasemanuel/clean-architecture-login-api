e9886a83fa99cf3145f39ad21f8ac0831d702401
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-06-25 14:26:11 -0300

    add routes file

---
+++ src/main/config/routes.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const fg = require('fast-glob')
+const router = require('express').Router()
+
+module.exports = app => {
+  app.use('/api', router)
+  fg.sync('**/src/main/routes/**.js').forEach(filePath => {
+    filePath = path.join('..', '..', '..', filePath)
+    require(filePath)(router)
+  })
+}
--- src/main/index.js
+++ src/main/index.js
@@ -2,8 +2,8 @@ const MongoHelper = require('../infra/helpers/mongo-helper')
 const env = require('./config/env')
 
 MongoHelper.connect(env.mongoUrl)
-  .then(() => {
-    const app = require('./config/app')
-    app.listen(3333, () => console.log('Server Running!'))
-  })
+  .then(() => {})
   .catch(console.error)
+
+const app = require('./config/app')
+app.listen(3333, () => console.log('Server Running!'))
