9f8f6d27aa26c1e288b7ff1cedddf903793a3220
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-06-28 10:26:34 -0300

    rename getDb to getCollection

--- src/infra/repositories/load-user-by-email-repository.test.js
+++ src/infra/repositories/load-user-by-email-repository.test.js
@@ -1,7 +1,7 @@
 const MongoHelper = require('../helpers/mongo-helper')
 const LoadUserByEmailRepository = require('./load-user-by-email-repository')
 const MissingParamError = require('../../utils/errors/missing-param-error')
-let db
+let userModel
 
 const makeSut = () => {
   return new LoadUserByEmailRepository()
@@ -10,10 +10,10 @@ const makeSut = () => {
 describe('LoadUserByEmail Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
-    db = await MongoHelper.getDb()
+    userModel = await MongoHelper.getCollection('users')
   })
   beforeEach(async () => {
-    await db.collection('users').deleteMany()
+    await userModel.deleteMany()
   })
   afterAll(async () => {
     await MongoHelper.disconnect()
@@ -26,7 +26,7 @@ describe('LoadUserByEmail Repository', () => {
   test('Should an return user if user is found', async () => {
     const sut = makeSut()
     const fakeUser = (
-      await db.collection('users').insertOne({
+      await userModel.insertOne({
         email: 'valid_email@email.com',
         password: 'hashed_password'
       })
